6d5834a64cc52cf7593ccaca612d851c6036b6d0
Emre <45405919+pwrdbypwnd@users.noreply.github.com>
2025-07-05 15:19:40 +0300

    Update locators.py

--- test/locators.py
+++ test/locators.py
@@ -1,12 +1,9 @@
 class InsiderLocators:
-    # Ana sayfa
     HOMEPAGE_LOGO = "//img[contains(@src, 'logo-old.png')]"
 
-    # Navigasyon menüsü
     COMPANY_MENU = "//a[text()='Company']"
     CAREERS_MENU = "//a[text()='Careers']"
 
-    # Kariyer sayfası blokları
     LOCATIONS_BLOCK = "//h2[text()='Locations']"
     TEAMS_BLOCK = "//h2[text()='Teams']"
     LIFE_AT_INSIDER_BLOCK = "//h2[text()='Life at Insider']"
